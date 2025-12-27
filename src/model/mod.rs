35ef0e142daa9772f39cbd2775940a195a010495
Mattias Jansson <mattias@janstall.se>
2025-12-27 23:18:44 +0000

    Remove obsolete and removed config fields

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -8,12 +8,9 @@ pub type UserId = String;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
-    pub company_onboard_complete_oauth_client_id: String,
     pub oauth_endpoint_url: String,
     pub oauth_default_client_id: String,
     pub oauth_default_redirect_uri: String,
-    pub oauth_grant_type: String,
-    pub oauth_response_type: String,
 }
 
 #[derive(Debug, Clone)]
