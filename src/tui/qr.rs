225c64791f1663c605717396a1756ec63c0eb675
Mattias Jansson <mattias.jansson@adabeat.com>
2026-05-05 13:49:58 +0200

    Go back to dark_color -> Light and vice versa
    Most terminals will use a dark background but the library assumes the text is
    dark, so they need to be flipped.

--- src/tui/qr.rs
+++ src/tui/qr.rs
@@ -5,5 +5,8 @@ use qrcode_unicode_ext::BraillePixel;
 pub fn encode(code_data: &String) -> Result<String, Error> {
     let code =
         QrCode::with_version(code_data, Version::Normal(11), EcLevel::H)?;
-    Ok(code.render::<BraillePixel>().build())
+    Ok(code.render::<BraillePixel>()
+            .dark_color(BraillePixel::Light)
+            .light_color(BraillePixel::Dark)
+            .build())
 }
